5d3dfae306822c687d160b546c81b6ec4f48e04c
Aluno <Aluno@ETEJK-LAB03-M05>
2024-08-06 21:28:24 -0300

    commit inicial

---
+++ moda_intima/src/main/java/br/com/moda_intima/ModaIntimaApplication.java
@@ -0,0 +1,13 @@
+package br.com.moda_intima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModaIntimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModaIntimaApplication.class, args);
+	}
+
+}
