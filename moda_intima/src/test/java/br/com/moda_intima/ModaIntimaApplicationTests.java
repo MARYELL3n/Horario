3819a28ffa3c9cba62dfc8192a6e12739cf01977
Aluno <Aluno@ETEJK-LAB03-M05>
2024-08-06 21:28:06 -0300

    commit inicial

---
+++ moda_intima/src/test/java/br/com/moda_intima/ModaIntimaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moda_intima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModaIntimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
